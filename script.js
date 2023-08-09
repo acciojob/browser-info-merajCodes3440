eaf435d8f95487f200263239ae2c7f23b8c88813
MerajCodes <merajsap184@gmail.com>
2023-08-09 17:10:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const browserName = navigator.appName;
+        const browserVersion = navigator.appVersion;
+
+        // Create a message to display
+        const message = `You are using ${browserName} (Version: ${browserVersion}).`;
+
+        // Get the browser-info div element
+        const browserInfoDiv = document.getElementById('browser-info');
+
+        // Create a new <p> element to hold the message
+        const messageElement = document.createElement('p');
+        messageElement.textContent = message;
+
+        // Append the message to the browser-info div
+        browserInfoDiv.appendChild(messageElement)
